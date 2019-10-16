a19a6ea46c4db6bec3bfce7218bba1c5d677ebb0
P. Rani Hooda <ranih1213@gmail.com>
2019-10-16 02:19:08 -0400

    Fully working and cleaned up code.

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
 
-# ...displays blog posts on a main page and allows users to add new blog posts on a form page. After submitting a 
-# new blog entry on the form page, the user is redirected to a page that displays only that blog (rather than 
-# returning to the form page or to the main page). Each blog post has a title and a body. 
+#ASSIGNMENT...displays blog posts on a main page and allows users to add new blog posts on a form page. After submitting a 
+#new blog entry on the form page, the user is redirected to a page that displays only that blog (rather than 
+#returning to the form page or to the main page). Each blog post has a title and a body. 
 
-# Make sure you can say the following about your app:
+#Make sure you can say the following about your app:
 
 #The /blog route displays all the blog posts.
 
@@ -21,7 +21,7 @@ db = SQLAlchemy(app)
 #main blog page.
 
 #You have two templates, one each for the /blog (main blog listings) and /newpost (post new blog entry) views.
-# Your templates should extend a base.html template which includes some boilerplate HTML that will be used on each page.
+#Your templates should extend a base.html template which includes some boilerplate HTML that will be used on each page.
 
 #In your base.html template, you have some navigation links that link to the main blog page and to the add new blog page.
 
@@ -30,20 +30,20 @@ db = SQLAlchemy(app)
 
 #TO DO Check get and post to see if it's correct
 
-#TO DO Let's add the ability to view each blog all by itself on a webpage. Instead of creating multiple HTML files, 
-# one for each new blog post we create, we can use a single template to display a given blog's title and body.
-# We'll designate which blog's data we want displayed by using a query param containing the id for that blog in the url. 
-# Then the request handler can dynamically grab the blog that corresponds to that id from the database and pass it to 
-# the template to generate the desired page.
+#Let's add the ability to view each blog all by itself on a webpage. Instead of creating multiple HTML files, 
+#one for each new blog post we create, we can use a single template to display a given blog's title and body.
+#We'll designate which blog's data we want displayed by using a query param containing the id for that blog in the url. 
+#Then the request handler can dynamically grab the blog that corresponds to that id from the database and pass it to 
+#the template to generate the desired page.
 
 #There are two use cases for this functionality:
 
 #Use Case 1: We click on a blog entry's title on the main page and go to a blog's individual entry page.
 
 #Use Case 2: After adding a new blog post, instead of going back to the main page, we go to that blog post's 
-# individual entry page.
+#individual entry page.
 #For both use cases we need to create the template for the page that will display an individual blog, so start 
-# by making that. All it needs do is display a blog title and blog body. 
+#by making that. All it needs do is display a blog title and blog body. 
 
 
 
@@ -83,57 +83,32 @@ to get the right blog object (the one that has the id we'll get from
 the url) from the database and into the render_template function call?"""
 
 
-#Could the below be combined with /blog route?
 @app.route('/', methods=['POST', 'GET'])
 def blog():
     blogs = Blog.query.all()
     return render_template('blog.html', title="Build a Blog", blogs=blogs)
 
-bid = "" #is this necessary?
 
 @app.route('/blog', methods=['POST', 'GET'])
 def index():
-    #blogid = int(request.form.get(['blog-id'], False)) #This will have a value at the end of the for loop
-   # blogs = Blog.query.filter_by(id=12).all()
-   # return render_template('blog.html', blogs=blogs
-    
-    #If there is a blog-id, do filter by else do all.
-    #blogs = Blog.query.filter_by(id=12).all() #This works! Just need to figure out how to pass in the correct ID
-    
-    #Supposed to use dictionary request.args here and see if returns something
-
-    bid = request.args.get('id') #This is OK
+    bid = request.args.get('id')
 
     if bid is None:
         blogs = Blog.query.all()
-        return render_template('blog.html', title="Build a Blog", blogs=blogs) #redirect("/blog?id=" + str(12)) Might have to change to redireect to blog?id=
-    else:
-        blogs = Blog.query.filter_by(id=bid).all() #This is working
-        return render_template('blog.html', blogs=blogs) #redirect("/blog?id=" + str(12)) Might have to change to redireect to blog?id=
-
-    #blogid = int(request.form['blog-id']) #This will have a value at the end of the for loop
-
-    """
-    Is the below even needed or correct?
-    if blogid == "":
-        #thi['blog_id']s part of the if is not executing ever, even when selecting a specific id = 
-        blogs = Blog.query.filter_by(id=blogid).all() #blogs = Blog.query.get(12)
         return render_template('blog.html', title="Build a Blog", blogs=blogs)
     else:
-        #Only grab the one blog?
-        blogs = Blog.query.all()
-        #blogs = Blog.query.get()
-        return render_template('blog.html', title="Build a Blog", blogs=blogs)"""
+        blogs = Blog.query.filter_by(id=bid).all()
+        return render_template('blog.html', blogs=blogs)
 
 
 #You've got to create a new database record for the blog entry that has just been submitted, but
-#  before redirecting, you need to grab the id for the record you just created. That way you'll 
-# know what url (i.e., what number to put on the right side of ?id=) to redirect the user to after 
-# they submit their new entry.
-# When you create a new blog post object, it doesn't immediately have an id that is not None. 
-# However, after running db.session.commit(), SQLAlchemy will populate the id property of the 
-# object to be the value auto-generated by the database upon insert. In other words, after you 
-# commit, you can get the id by using the id property of the object you just saved.
+#before redirecting, you need to grab the id for the record you just created. That way you'll 
+#know what url (i.e., what number to put on the right side of ?id=) to redirect the user to after 
+#they submit their new entry.
+#When you create a new blog post object, it doesn't immediately have an id that is not None. 
+#However, after running db.session.commit(), SQLAlchemy will populate the id property of the 
+#object to be the value auto-generated by the database upon insert. In other words, after you 
+#commit, you can get the id by using the id property of the object you just saved.
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def new_post():
@@ -158,55 +133,15 @@ def new_post():
             
             bid = new_blog.id
 
-            #blogid = request.args.get['blog.id'] #This causes an error
     else:   
         return render_template('newpost.html', title='Add a New Blog Entry')
 
     if (title_error or body_error):
         return render_template('newpost.html', title='Add a New Blog Entry', title_error=title_error, body_error=body_error)
+
     else:
-        #blogs = Blog.query.all() #This is where it should show only the one blog post just submitted
-        return redirect('/blog?id={0}'.format(bid) )#'/blog?id={0}'.format(blogs)) #Fix this
-        #return render_template('blog.html', title="Build a Blog", blogs=blogs)
+        return redirect('/blog?id={0}'.format(bid) )
 
-
-#########################################################################################
-#THIS FUNCTION IS LIKELY UNNECESSARY
-#def displaypost
-#need to ultimately pass in something like http://localhost:5000/hello?first_name=Chris
-#like  return redirect("/welcome?username=" + username)
-@app.route('/displaypost', methods=['POST', 'GET'])
-def displaypost(): #maybe have to get new_blog passed in from new post function?
-    #newblog = Blog.query(id=1) #what's the syntax to query what we just added, maybe just need the id?
-    blogs = Blog.query.all()
-    blogid = str(blogs[3].id) #This is returning the third item in the list. We want the specific one we just created
-    #blogid = request.form['blog_id']
-
-    #blogs = Blog.query.get(blogid) #this will give the specific blog asking for
-    #db.session.commit() #is this needed to completel the query?
-
-    #How to find the id for the blog we just created?
-    
-
-    return redirect("/blogpost?id=" + blogid) #something is off here with how to display the page without .html
-
-#THIS FUNCTION IS LIKELY UNNECESSARY
-@app.route("/blogpost")
-def welcome():
-    
-    #if request.method == "POST":
-    #    blogid = request.form['blog']
-
-    blogs = Blog.query.get(17)
-    
-    #blogid = blogs[3].id
-    
-    blogtitle = blogs.title
-    blogbody = blogs.body
-
-    return render_template('blogpost.html', title=blogtitle, body=blogbody)
-
- ####################   
 
 
 if __name__ == '__main__':
